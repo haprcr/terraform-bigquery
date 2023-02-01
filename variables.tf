9f5804058c8325727c9f276cbef5bd80da6af6bd
haprcr <124026578+haprcr@users.noreply.github.com>
2023-02-01 15:06:33 +0530

    updated variable.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "location" {
 variable "delete_contents_on_destroy" {
   description = "(Optional) If set to true, delete all the tables in the dataset when destroying the resource; otherwise, destroying the resource will fail if tables are present."
   type        = bool
-  default     = null
+  default     = false
 }
 
 variable "deletion_protection" {
